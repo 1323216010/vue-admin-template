ff1d0ab553c7719ba0733281181bd442dd2eb93e
13232160100 <ypc0100@qq.com>
2022-11-12 20:58:38 +0800

    233

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,5 +1,5 @@
   // 定义一些公共的属性和方法
-  const kkIp = 'http://127.0.0.1:8012'
+  const kkIp = 'http://120.46.176.87:8012'
   const fileUpload = kkIp + '/fileUpload';
   const onlinePreview = kkIp + '/onlinePreview?url=';
   const downloadFile = kkIp + '/demo/'
